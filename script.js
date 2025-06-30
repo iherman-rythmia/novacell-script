26527a8d424738c01d04fc77a2ea6fdcb5a6cf7d
Mike Stairs <163016496+mike-rythmia@users.noreply.github.com>
2025-06-30 11:59:28 -0400

    remove duplicate lead_id

--- script.js
+++ script.js
@@ -13,26 +13,23 @@ const country = urlParams.get('country');
 const state = urlParams.get('state');
 const postal_code = urlParams.get('postal_code');
 
-
-const lead_id='abcdefg111';
-
 function processSubmit(){
     const choice = document.querySelector('input[name="choice"]:checked').value;
     let promo = document.getElementById("promo").value;
 
     if (promo !== ''){
-    if (promo.search(/STEM\d+/) !== 0){
-        document.getElementById("errors").innerHTML = "You must provide a valid promo code";
-        document.getElementById("errors").style.color = 'red';
-        return;
-    } else {
-        promo = promo.replace('STEM','');
-    }
-    if (promo < 0){
-        document.getElementById("errors").innerHTML = "You must provide a valid promo code";
-        document.getElementById("errors").style.color = 'red';
-        return;
-    }
+        if (promo.search(/STEM\d+/) !== 0){
+            document.getElementById("errors").innerHTML = "You must provide a valid promo code";
+            document.getElementById("errors").style.color = 'red';
+            return;
+        } else {
+            promo = promo.replace('STEM','');
+        }
+        if (promo < 0){
+            document.getElementById("errors").innerHTML = "You must provide a valid promo code";
+            document.getElementById("errors").style.color = 'red';
+            return;
+        }
     } else {
         promo = 0;
     }
@@ -51,7 +48,7 @@ function processSubmit(){
         "email": email
     };
     
-        sendInvoice(choice, billing_info, lead_id, promo);
+    sendInvoice(choice, billing_info, lead_id, promo);
     
 }
 
@@ -76,36 +73,36 @@ async function sendInvoice(choice, billing_info, lead_id, promo){
     console.log(items,','+amount);
 
     try{
-    const res = await fetch("https://fluidpay-test-api.azurewebsites.net/api/sendInvoice",
+        const res = await fetch("https://fluidpay-test-api.azurewebsites.net/api/sendInvoice",
         {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
-            "customer_id":lead_id,
-            "billing_address":billing_info,
-            "items": items,
-            "amount":amount,
-            "invoice_number": '',
-            "charge_type":"stem",
-            "message": "This is your online payment form for your NovaCell treatment . "
-        })
-    });
+                "customer_id":lead_id,
+                "billing_address":billing_info,
+                "items": items,
+                "amount":amount,
+                "invoice_number": '',
+                "charge_type":"stem",
+                "message": "This is your online payment form for your NovaCell treatment . "
+            })
+        });
 
-    const resp2 = await res.json();
-    if (res.status === 200){
-    document.getElementById("errors").innerHTML = "Invoice sent successfully";
-    document.getElementById("errors").style.color = 'green';
-    document.getElementById("submit").style.display = "none";
-    document.getElementById("container").style.display = "none";
-} else {
-    document.getElementById("errors").innerHTML = resp2.msg;
-    document.getElementById("errors").style.color = 'red';
-}
-}
-    catch(e) {
-    console.log('error fetching data:',JSON.stringify(e));
-    document.getElementById("error").innerHTML = JSON.stringify(e);
-}
+        const resp2 = await res.json();
+        if (res.status === 200){
+            document.getElementById("errors").innerHTML = "Invoice sent successfully";
+            document.getElementById("errors").style.color = 'green';
+            document.getElementById("submit").style.display = "none";
+            document.getElementById("container").style.display = "none";
+        } else {
+            document.getElementById("errors").innerHTML = resp2.msg;
+            document.getElementById("errors").style.color = 'red';
+        }
+    }
+        catch(e) {
+        console.log('error fetching data:',JSON.stringify(e));
+        document.getElementById("error").innerHTML = JSON.stringify(e);
+    }
 }
 
 console.log("test");
