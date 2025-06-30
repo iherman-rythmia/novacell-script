3cdc9cc78da2b5e269c8979846e0d7806f6c80f6
Mike Stairs <163016496+mike-rythmia@users.noreply.github.com>
2025-06-30 11:41:01 -0400

    added input parameters

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
 
-    const lead_id='abcdefg111';
+const queryString = window.location.search; 
+const urlParams = new URLSearchParams(queryString);
 
-    function processSubmit(){
+const lead_id = urlParams.get('lead_id');
+const first_name = urlParams.get('first_name');
+const last_name = urlParams.get('last_name');
+const email = urlParams.get('email');
+const phone= urlParams.get('phone');
+const address_line_1 = urlParams.get('address_line_1');
+const city = urlParams.get('city');
+const country = urlParams.get('country');
+const state = urlParams.get('state');
+const postal_code = urlParams.get('postal_code');
+
+
+const lead_id='abcdefg111';
+
+function processSubmit(){
     const choice = document.querySelector('input[name="choice"]:checked').value;
     let promo = document.getElementById("promo").value;
 
     if (promo !== ''){
     if (promo.search(/STEM\d+/) !== 0){
-    document.getElementById("errors").innerHTML = "You must provide a valid promo code";
-    document.getElementById("errors").style.color = 'red';
-    return;
-} else {
-    promo = promo.replace('STEM','');
-}
+        document.getElementById("errors").innerHTML = "You must provide a valid promo code";
+        document.getElementById("errors").style.color = 'red';
+        return;
+    } else {
+        promo = promo.replace('STEM','');
+    }
     if (promo < 0){
-    document.getElementById("errors").innerHTML = "You must provide a valid promo code";
-    document.getElementById("errors").style.color = 'red';
-    return;
-
-}
-} else {
-    promo = 0;
-}
+        document.getElementById("errors").innerHTML = "You must provide a valid promo code";
+        document.getElementById("errors").style.color = 'red';
+        return;
+    }
+    } else {
+        promo = 0;
+    }
 
     const billing_info = {
-    "id":null,
-    "first_name": 'Joe',
-    "last_name": 'Blow',
-    "address_line_1":'123 Somewhere Dr',
-    "address_line_2": '',
-    "city": 'Bala',
-    "state": 'ON',
-    "postal_code": 'P0A1A1',
-    "country": 'CA',
-    "phone": '6472233061',
-    "email": 'michael.stairs+nova@gmail.com'
-};
-
-    sendInvoice(choice, billing_info, lead_id, promo);
-
+        "id":null,
+        "first_name": first_name,
+        "last_name": last_name,
+        "address_line_1":address_line_1,
+        "address_line_2": '',
+        "city": city,
+        "state": state,
+        "postal_code": postal_code,
+        "country": country,
+        "phone": phone,
+        "email": email
+    };
+    
+        sendInvoice(choice, billing_info, lead_id, promo);
+    
 }
 
 
-    async function sendInvoice(choice, billing_info, lead_id, promo){
+async function sendInvoice(choice, billing_info, lead_id, promo){
     const obj = choice.split(',');
     const line_item = parseInt(obj[1]+obj[2]);
 
@@ -50,32 +64,32 @@
     const amount = promo > 0 ? line_item - discount : line_item;
 
     const items =  [{
-    "name": "NovaCell payment",
-    "description": `Payment for ${obj[0]} of stem cells`,
-    "quantity": 1,
-    "unit_price": line_item,
-    "discount_amount": discount,
-    "taxable": false,
-    "status": "pending"
-}];
+        "name": "NovaCell payment",
+        "description": `Payment for ${obj[0]} of stem cells`,
+        "quantity": 1,
+        "unit_price": line_item,
+        "discount_amount": discount,
+        "taxable": false,
+        "status": "pending"
+    }];
 
     console.log(items,','+amount);
 
     try{
     const res = await fetch("https://fluidpay-test-api.azurewebsites.net/api/sendInvoice",
-{
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-    "customer_id":lead_id,
-    "billing_address":billing_info,
-    "items": items,
-    "amount":amount,
-    "invoice_number": '',
-    "charge_type":"stem",
-    "message": "This is your online payment form for your NovaCell treatment . "
-})
-});
+        {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+            "customer_id":lead_id,
+            "billing_address":billing_info,
+            "items": items,
+            "amount":amount,
+            "invoice_number": '',
+            "charge_type":"stem",
+            "message": "This is your online payment form for your NovaCell treatment . "
+        })
+    });
 
     const resp2 = await res.json();
     if (res.status === 200){
